a5e829f2ba69af66793a51d10992ad67fd729056
Jeffrey Wan <Jwan622@gmail.com>
2015-01-21 14:46:08 -0700

    fixed items database

---
+++ db/migrate/20150120204409_item.rb
@@ -0,0 +1,4 @@
+class Item < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121183735) do
+ActiveRecord::Schema.define(version: 20150121213551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,12 @@ ActiveRecord::Schema.define(version: 20150121183735) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "cost"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.integer  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
+    t.string   "photo"
   end
 
   create_table "items_categories", force: :cascade do |t|
