723c52f663a8ff8998747810765b3e8fb0385a24
Orion Osborn <orion.osborn@gmail.com>
2015-01-23 23:40:33 -0700

    update cart tests and refactor

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,29 @@
 class CartsController < ApplicationController
-  def update
-    session[:cart] = { params[:id] => params[:item][:quantity] }
+  def create
+    update_cart
     flash[:notice] = 'Added to Cart'
-    redirect_to item_path(params[:id])
+    redirect_to items_path
+  end
+
+  def showcart
+    @cart = Cart.new(session[:cart]) if session[:cart]
+  end
+
+  private
+
+  def update_cart
+    if session[:cart]
+      update_existing_cart
+    else
+      session[:cart] = { params[:cart][:id] => params[:cart][:quantity] }
+    end
+  end
+
+  def update_existing_cart
+    if session[:cart][params[:cart][:id]]
+      session[:cart][params[:cart][:id]] = (session[:cart][params[:cart][:id]].to_i + params[:cart][:quantity].to_i).to_s
+    else
+      session[:cart][params[:cart][:id]] = params[:cart][:quantity].to_s
+    end
   end
 end
