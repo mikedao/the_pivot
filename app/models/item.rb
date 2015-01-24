b6e32ecdc53c11a886275ad8f6db64e57ed5e9f7
Jeffrey Wan <Jwan622@gmail.com>
2015-01-24 12:34:52 -0700

    fixing the validates for items so that it has to have a category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ActiveRecord::Base
-  validates :description, length: { in: 0..255, allow_nil: false }, presence: true
-  validates :title, presence: true, uniqueness: true, allow_blank: false
-  validates :price, presence: true, :numericality => { :only_integer => true }
-
   has_many :items_categories
   has_many :categories, through: :items_categories
 
   has_many :item_orders
   has_many :orders, through: :item_orders
+
+  validates :description, length: { in: 0..255, allow_nil: false }, presence: true
+  validates :title, presence: true, uniqueness: true, allow_blank: false
+  validates :price, presence: true, :numericality => { :only_integer => true }
 
   def quantity
 
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -4,46 +4,57 @@ class ItemTest < ActiveSupport::TestCase
   attr_reader :item
 
   test 'it is valid' do
-    @item = Item.create(title: 'espresso', description: "this is black gold", price: 30000)
+    category = Category.create(name: "hot beverages")
+    item = category.items.create(title: 'espresso', description: "this is black gold", price: 30000)
     assert item.valid?
   end
 
-  test 'it cannot have an empty string as a name' do
-    @item = Item.create(title: '', description: "this is black gold", price: 30000)
+  test 'it cannot have an empty string as a title' do
+    category = Category.create(name: "hot beverages")
+    item = category.items.create(title: '', description: "this is black gold", price: 30000)
     assert item.invalid?
   end
 
   test "it cannot have an empty string as a description" do
-    @item = Item.create(title: 'espresso', description: "", price: 30000)
+    category = Category.create(name: "hot beverages")
+    item = category.items.create(title: 'espresso', description: "", price: 30000)
     assert item.invalid?
   end
 
   test "it cannot have a duplicate title" do
-    @item = Item.create(title: 'espresso', description: "this is black gold", price: 20000)
-    @item1 = Item.create(title: 'espresso', description: "this is bleck gold", price: 30000)
-    @item2 = Item.create(title: 'espresso', description: "this is back gold", price: 30000)
+    category = Category.create(name: "hot beverages")
+    category.items.create(title: 'espresso', description: "this is black gold", price: 30000)
+    category.items.create(title: 'espresso', description: "this is bleck gold", price: 30000)
+    category.items.create(title: 'espresso', description: "this is back gold", price: 30000)
     assert_equal 1, Item.where(:title => "espresso").count
   end
 
   test "it has a valid price" do
-    @item1 = Item.create(title: 'hot cocoa', description: "this is black gold", price: 1000)
-    @item2 = Item.create(title: 'italian roast', description: "this is blak gold", price: "10.00z")
-    @item3 = Item.create(title: 'decaf something or other', description: "this is blakc gold", price: ".01")
-    @item4 = Item.create(title: 'sludge', description: "this is blakc gold", price: 0.01)
-    @item5 = Item.create(title: 'just straight espresso beans in a cup', description: "this is blakc gold")
+    category = Category.create(name: "hot beverages")
+    item1 = category.items.create(title: 'espresso', description: "this is black gold", price: 30000)
+    item2 = category.items.create(title: 'italian roast', description: "this is blak gold", price: "10.00z")
+    item3 = category.items.create(title: 'decaf something or other', description: "this is blakc gold", price: ".01")
+    item4 = category.items.create(title: 'sludge', description: "this is blakc gold", price: 0.01)
+    item5 = category.items.create(title: 'just straight espresso beans in a cup', description: "this is blakc gold")
 
-    assert @item1.valid?
-    refute @item2.valid?
-    refute @item3.valid?
-    refute @item4.valid?
-    refute @item5.valid?
+    assert item1.valid?
+    refute item2.valid?
+    refute item3.valid?
+    refute item4.valid?
+    refute item5.valid?
   end
 
 
   test 'it can have many categories' do
-    @item = Item.create(title: 'espresso', description: "this is black gold", price: 30000)
-    @category1 = @item.categories.create(name: 'hot beverages')
-    @category2 = @item.categories.create(name: 'cold beverages')
+    category = Category.create(name: "hot beverages")
+    item = category.items.create(title: 'espresso', description: "this is black gold", price: 30000)
+    category1 = item.categories.create(name: 'cold beverages')
     assert_equal 2, item.categories.count
+  end
+
+  test "it has to have at least a category" do
+    category = Category.create(name: "hot beverages")
+    item = category.items.create(title: 'espresso', description: "this is black gold", price: 30000)
+    assert item.categories
   end
 end
