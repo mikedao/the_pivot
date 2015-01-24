83b3c546a1f41e190e16dbbf135b633f001f55bb
Lydias303 <shaferlydia@yahoo.com>
2015-01-24 11:57:36 -0700

    fized merge issue

--- test/integration/admin_login_test.rb
+++ test/integration/admin_login_test.rb
@@ -46,7 +46,6 @@ class AdminUserTest < ActionDispatch::IntegrationTest
 
   test "registered admin can see create category on menu page" do
     skip
->>>>>>> a40d29e3d65a751e41bf529b1af8794e080eeabd
     ApplicationController.any_instance.stubs(:current_user).returns(user)
     visit root_path
     click_link_or_button('Admin Dashboard')
