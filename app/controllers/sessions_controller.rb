873054358df9d6b82e38e2c5e41e45e07d3c3084
Jeffrey Wan <Jwan622@gmail.com>
2015-01-25 10:55:50 -0700

    tests working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      flash[:notice] = "Welcome back!"
       redirect_to root_url
     else
       flash[:errors] = 'Invalid Login'
