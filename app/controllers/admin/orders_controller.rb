e658cc9a509496c6ab2db0b0f795b22a133c3aba
Scott Crawford <scottcrawford03@gmail.com>
2015-01-29 10:07:56 -0700

    changed retired select form and status select form and divs

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,8 +4,8 @@ class Admin::OrdersController < Admin::BaseController
   end
 
   def update
-    order = Order.find(params[:update_order_status][:order_id])
-    order.update(status: params[:update_order_status][:status])
+    order = Order.find(params[:id])
+    order.update(status: params[:update_order_status]["status_#{order.id}"])
     redirect_to admin_orders_url
   end
 end
