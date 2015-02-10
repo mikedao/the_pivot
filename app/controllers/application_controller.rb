c4d21577604cfd5968a0460fc7b83e92dde4f198
Orion Osborn <orion.osborn@gmail.com>
2015-02-10 15:45:36 -0700

    fixed white space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to login_path if current_user.nil?    
+    redirect_to login_path if current_user.nil?   
   end
 end
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,14 +1,4 @@
 class ItemOrder < ActiveRecord::Base
   belongs_to :item
   belongs_to :order
-
-  # def quantity
-  #   order.items.group_by {|}
-  #   count
-  # end
-  #
-  # # def line_item_cost
-  # #
-  # # end
-
 end
