70ffe7790cd996828ceb135cb26866c6758ad176
Michael Dao <michael.dao@gmail.com>
2015-02-17 21:01:02 -0700

    Added basic categories functionality for platform admin.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,4 +3,30 @@ class Admin::CategoriesController < Admin::BaseController
   def index
     @categories = Category.all
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.valid?
+      category_create
+    else
+      invalid_category
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:categories).permit(:name)
+  end
+
+  def category_create
+    @category.save
+    flash[:notice] = "Category Created"
+    redirect_to admin_categories_path
+  end
+
+  def invalid_category
+    flash[:errors] = "Please Try Again"
+    redirect_to admin_categories_path
+  end
 end
