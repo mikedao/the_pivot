fd004957a5421796bff4e648fd0a113dec0ceb1e
Orion Osborn <orion.osborn@gmail.com>
2015-02-19 15:29:11 -0700

    add test to ensure checkout notice exists

---
+++ test/integration/authenticated_lender_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class AuthenticatedLenderTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  test "authed lender with no pending loans is not shown a call to action" do
+    user = create(:user)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit root_path
+
+    refute page.has_content?("Help someone today and")
+    refute page.has_content?("complete your loan")
+  end
+
+  test "authed lender with pending loans is shown a call to action" do
+    user = create(:user)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    #click lend somewhere
+
+    visit root_path
+
+    assert page.has_content?("Help someone today and")
+    assert page.has_content?("complete your loan")
+  end
+end
