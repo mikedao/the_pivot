d51b9d5e32a7797f5fe2ca6f1cf248b51018392c
Michael Dao <michael.dao@gmail.com>
2015-02-19 15:19:19 -0700

    Reworking user signup

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -9,15 +9,15 @@ class TenantsController < ApplicationController
   end
 
   def create
-    if Tenant.find_by(organization: params[:tenant_signup][:organization])
+    if Tenant.find_by(org_check)
       flash[:notice] = "Organization Already Exists"
       redirect_to new_tenant_path
     else
       tenant = Tenant.new(org_params)
       if tenant.valid?
         tenant.save
-        session[:tenant_id] = tenant.id
-        redirect_to new_user_path
+        @current_user.tenant_id = tenant.id
+        redirect_to root_path
         flash[:notice] = "Organization Created."
       else
         flash[:notice] = "Please try again."
@@ -30,5 +30,9 @@ class TenantsController < ApplicationController
 
   def org_params
     params.require(:tenant_signup).permit(:location, :organization)
+  end
+
+  def org_check
+    params.require(:tenant_signup).permit(:organization)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,14 +39,23 @@ class UsersController < ApplicationController
   def create_user(user)
     user.save
     session[:user_id] = user.id
+    puts "AM I A TENANT? #{tenant?}"
     send_welcome_email(user)
     flash[:notice] = "Thank you for creating an account."
-    redirect_to root_path
+    if tenant?
+      redirect_to new_tenant_path
+    else
+      redirect_to root_path
+    end
   end
 
   def send_welcome_email(user)
     UserMailer.welcome_borrower(user).deliver_now if user.borrower?
     UserMailer.welcome_lender(user).deliver_now if user.lender?
+  end
+
+  def tenant?
+    params[:signup][:tenant] == 1
   end
 
   def user_params
