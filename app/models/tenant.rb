60b03e01654fa30922c2b8318f89839c8d4e0cdd
Michael Dao <michael.dao@gmail.com>
2015-02-20 09:34:50 -0700

    Added default values to Tenant model.

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -4,11 +4,17 @@ class Tenant < ActiveRecord::Base
   has_many :users
 
   before_save :create_slug
+  after_initialize :defaults
 
   validates :location,     presence: true, length: { in: 6..255 }
   validates :organization, presence: true, length: { in: 4..255 }
 
   def create_slug
     self.slug = organization.parameterize
+  end
+
+  def defaults
+    self.status ||= false
+    self.approval ||= false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 20150219204048) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "slug"
+    t.boolean  "status"
+    t.boolean  "approval"
   end
 
   create_table "users", force: :cascade do |t|
