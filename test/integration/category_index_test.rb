10aadaa3adb49b4430dc1c05df5a3f6094463f2c
Adam Smith <adamsmith910@aol.com>
2015-02-22 11:27:24 -0700

    tabs

--- test/integration/category_index_test.rb
+++ test/integration/category_index_test.rb
@@ -6,22 +6,22 @@ class CategoryIndexTest < ActionDispatch::IntegrationTest
 
   test "Categories index page does not display a category
   that has no associated projects" do
-  	category = create(:category)
+    category = create(:category)
 
-  	visit root_path
+    visit root_path
 
-  	refute page.has_content?(category.name)
+    refute page.has_content?(category.name)
   end
 
   test "Categories index page does display a category that
   has an associated project" do
-  	# the factory for project automatically creates an associated 
-  	# category
-  	project = create(:project)
-  	category = project.categories.first
+    # the factory for project automatically creates an associated 
+    # category
+    project = create(:project)
+    category = project.categories.first
 
-  	visit root_path
+    visit root_path
 
-  	assert page.has_content?(category.name)
+    assert page.has_content?(category.name)
   end
 end
