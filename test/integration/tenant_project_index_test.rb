b9b5dc467a33b6d0525216603c031d29837f38c8
Adam Smith <adamsmith910@aol.com>
2015-02-24 18:03:35 -0700

    new passing test for tenant project index test

--- test/integration/tenant_project_index_test.rb
+++ test/integration/tenant_project_index_test.rb
@@ -19,4 +19,20 @@ class TenantProjectIndexTest < ActionDispatch::IntegrationTest
 		assert page.has_content?("Next")
 	end
 
+	test "when user clicks on 'Next' button they are taken to
+	the next 10 projects in the index" do
+		authenticated_user = create(:user)
+		ApplicationController.any_instance.stubs(:current_user).
+			returns(authenticated_user)
+		tenant = create(:tenant)
+		15.times do
+			tenant.projects << create(:project)
+		end
+
+		visit tenant_projects_path(slug: tenant.slug)
+		click_link_or_button("Next", match: :first)
+
+		assert page.has_content?("De Beers17")
+	end
+
 end
