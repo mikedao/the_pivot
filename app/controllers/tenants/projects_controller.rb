dfe927409b3c8dae372fc5cebd5fa2086413cbef
Adam Smith <adamsmith910@aol.com>
2015-02-24 17:18:40 -0700

    started pagination for tenant projects controller

--- app/controllers/tenants/projects_controller.rb
+++ app/controllers/tenants/projects_controller.rb
@@ -6,7 +6,7 @@ class Tenants::ProjectsController < ApplicationController
     redirect_to root_path if @tenant.nil?
 
     if params[:category_name] == "Shop All" || params[:category_name].nil?
-      @projects = Project.includes(:photos).active
+      @projects = Project.paginate(page: params[:page], :per_page => 10).includes(:photos).active
     else
       @projects = Category.find_by(name: params[:category_name]).projects
     end
