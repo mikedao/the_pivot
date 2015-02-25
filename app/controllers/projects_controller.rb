1c4011519a0fda5be0111bd0d81b33597b04c091
Adam Smith <adamsmith910@aol.com>
2015-02-25 11:17:16 -0700

    tests passing but no actual functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,14 +4,18 @@ class ProjectsController < ApplicationController
       project.tenant.visible_to_lenders == true &&
       project.current_amount_needed > 0
     end
+    # paginate_projects
     @categories = Category.all
   end
 
   private
 
   def all_projects
-    Project.paginate(page: params[:page],
-                     per_page: 10)
-                    .includes(:categories).active.joins(:tenant)
+    Project.includes(:categories).active.joins(:tenant)
   end
+
+  # def paginate_projects
+  #   @projects = Project.paginate(page: params[:page],
+  #                                per_page: 10)
+  # end
 end
