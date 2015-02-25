6d2557c8e8ba55965fc193c2a2bf4147400f1237
Adam Smith <adamsmith910@aol.com>
2015-02-24 18:58:21 -0700

    hound

--- test/integration/project_index_test.rb
+++ test/integration/project_index_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class ProjectIndexTest < ActionDispatch::IntegrationTest
-	include Capybara::DSL
-	include FactoryGirl::Syntax::Methods
+  include Capybara::DSL
+  include FactoryGirl::Syntax::Methods
 
-	test "pagination renders on page" do
-		15.times do 
+  test "pagination renders on page" do
+		15.times do
 			create(:project)
 		end
 
@@ -15,7 +15,7 @@ class ProjectIndexTest < ActionDispatch::IntegrationTest
 		assert page.has_content?("Previous")
 	end
 
-	test "when user clicks on 'Next' button they are taken to
+  test "when user clicks on 'Next' button they are taken to
 	the next 10 projects in the index" do
 		15.times do
 			create(:project)
@@ -27,7 +27,7 @@ class ProjectIndexTest < ActionDispatch::IntegrationTest
 		assert "/projects?page=2", current_path
 	end
 
-	test "when user clicks on 'Previous' button they are taken back
+  test "when user clicks on 'Previous' button they are taken back
 	to the previous 10 projects" do
 		15.times do
 			create(:project)
