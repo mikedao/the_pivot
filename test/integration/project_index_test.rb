048c56d07a039aa8e036f392b77700987390a066
Adam Smith <adamsmith910@aol.com>
2015-02-24 18:47:03 -0700

    all tests passing

--- test/integration/project_index_test.rb
+++ test/integration/project_index_test.rb
@@ -15,4 +15,29 @@ class ProjectIndexTest < ActionDispatch::IntegrationTest
 		assert page.has_content?("Previous")
 	end
 
+	test "when user clicks on 'Next' button they are taken to
+	the next 10 projects in the index" do
+		15.times do
+			create(:project)
+		end
+
+		visit projects_path
+		click_link_or_button("Next", match: :first)
+
+		assert "/projects?page=2", current_path
+	end
+
+	test "when user clicks on 'Previous' button they are taken back
+	to the previous 10 projects" do
+		15.times do
+			create(:project)
+		end
+
+		visit projects_path
+		click_link_or_button("Next", match: :first)
+		click_link_or_button("Previous", match: :first)
+
+		assert "/projects?page=1", current_path
+	end
+
 end
